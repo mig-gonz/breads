58239ed0fdd0c0b5d80a975235935770a93a31cd
Miguel <itsmig@proton.me>
2023-02-12 09:55:00 -0800

    sending index show data

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 
+// MIDDLEWARE
+app.use(express.static("public"));
+
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to an Awesome App about Breads");
@@ -21,6 +24,11 @@ app.get("/", (req, res) => {
 // Breads
 const breadsController = require("./controllers/bread_controllers.js");
 app.use("/breads", breadsController);
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 // listen
 app.listen(PORT, () => {
